1f3d39456bd13652d24f423ee3aeafeaab7d6cd7
t-j <t-j@terence-john.co.uk>
2020-12-03 17:13:29 +0000

    Init commit of 02-i

---
+++ 02-ProgramAlarm/IntCode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+namespace _02_ProgramAlarm
+{
+    public class IntCode
+    {
+        List<long> Memory;
+
+        public IntCode(List<long> memory)
+        {
+            Memory = memory;
+        }
+
+        public void Run()
+        {
+            int sp = 0;
+            while (Memory[sp] != 99)
+            {
+                int oper1, oper2, oper3;
+                switch (Memory[sp])
+                {
+                    case 1: // -- Add
+                        oper1 = (int)Memory[sp + 1];
+                        oper2 = (int)Memory[sp + 2];
+                        oper3 = (int)Memory[sp + 3];
+                        Memory[oper3] = Memory[oper1] + Memory[oper2];
+                        sp += 4;
+                        break;
+
+                    case 2: // -- Mult
+                        oper1 = (int)Memory[sp + 1];
+                        oper2 = (int)Memory[sp + 2];
+                        oper3 = (int)Memory[sp + 3];
+                        Memory[oper3] = Memory[oper1] * Memory[oper2];
+                        sp += 4;
+                        break;
+
+                    default:
+                        System.Console.WriteLine("*************BUGGER!!");
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ 02-ProgramAlarm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace _02_ProgramAlarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------ Acquire input ------------------------------------
+            List<long> memory = new List<long>();
+
+            string line = File.ReadAllText(@"Resources/input.txt");
+            //            line = "1,9,10,3,2,3,11,0,99,30,40,50";
+            foreach (var str in line.Split(new char[] { ',' }))
+            {
+                memory.Add(long.Parse(str));
+            }
+            memory[1] = 12;
+            memory[2] = 2;
+
+            //------------------------------ Part 1 ------------------------------------
+            System.Console.WriteLine("--------------------- Part 1 ---------------------------");
+            {
+                IntCode computer = new IntCode(memory);
+
+                computer.Run();
+
+                System.Console.WriteLine(memory[0]);
+            }
+            System.Console.WriteLine();
+
+            //------------------------------ Part 2 ------------------------------------
+            System.Console.WriteLine("--------------------- Part 2 ---------------------------");
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("--------------------- END ---------------------------");
+
+        }
+    }
+}
