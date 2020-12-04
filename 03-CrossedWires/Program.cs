f5a62599285610d1f5dc11e8ae107a56d1c03408
t-j <t-j@terence-john.co.uk>
2020-12-04 08:14:53 +0000

    part way through 03-i

---
+++ 03-CrossedWires/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace _03_CrossedWires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------ Acquire input ------------------------------------
+            List<Wire> wires = new List<Wire>();
+            string[] input = File.ReadAllLines(@"Resources/input.txt");
+
+            foreach (string str in input)
+            {
+                wires.Add(new Wire(str));
+            }
+
+            //------------------------------ Part 1 ------------------------------------
+            System.Console.WriteLine("--------------------- Part 1 ---------------------------");
+            {
+                int i = 0;
+
+                int? minDistance = null;
+
+                foreach (var pos in wires[0].Positions)
+                {
+                    Console.Write($"{++i,6} {DateTime.Now}\r");
+                    if (i % 1000 == 0)
+                        Console.WriteLine();
+                    if (wires[1].Positions.Contains(pos))
+                    {
+                        int distance = Math.Abs(pos.X) + Math.Abs(pos.Y);
+
+                        if (!minDistance.HasValue)
+                            minDistance = distance;
+                        else if (minDistance > distance)
+                            minDistance = distance;
+                    }
+                }
+                System.Console.WriteLine(minDistance);
+            }
+
+            //------------------------------ Part 2 ------------------------------------
+            System.Console.WriteLine("--------------------- Part 2 ---------------------------");
+            {
+            }
+            System.Console.WriteLine("\n--------------------- END ---------------------------");
+
+        }
+    }
+}
---
+++ 03-CrossedWires/Wire.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace _03_CrossedWires
+{
+    public class Wire
+    {
+        public List<Point> Positions;
+
+        public Wire(string str)
+        {
+            Positions = new List<Point>();
+
+            int x = 0, y = 0;
+            int xdelta = 0, ydelta = 0;
+
+            foreach (var s in str.Split(","))
+            {
+                char direction = s[0];
+                switch (direction)
+                {
+                    case 'U':
+
+                        xdelta = 0;
+                        ydelta = 1;
+                        break;
+
+                    case 'D':
+                        xdelta = 0;
+                        ydelta = -1;
+                        break;
+
+                    case 'R':
+                        xdelta = 1;
+                        ydelta = 0;
+                        break;
+
+                    case 'L':
+                        xdelta = -1;
+                        ydelta = 0;
+                        break;
+
+                    default:
+                        System.Console.WriteLine("*************WHAT!!!!");
+                        return;
+                }
+
+                int length = int.Parse(s.Substring(1));
+                for (int i = 0; i < length; i++)
+                {
+                    x += xdelta;
+                    y += ydelta;
+                    Positions.Add(new Point(x, y));
+                }
+            }
+        }
+    }
+}
